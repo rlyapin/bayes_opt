4b5347fe407bf04abd0c550e7317fd81b1af23d8
Roman Lyapin <lyapin.rk@gmail.com>
2018-05-08 22:14:55 +0200

    Added a collection of test functions to optimize

--- data_generator_2d.py
+++ data_generator_2d.py
@@ -1,23 +1,62 @@
 import numpy as np
+from sample_functions_2d import *
 
 class DataGenerator2D:
-	# Main class to hold signal generating object
-	# It is supposed to hold objects from here: https://www.sfu.ca/~ssurjano/optimization.html
-	# It is assumed the function will hold its minimum of 0 and maximum of 1 
-	# The domain of the function would be [0, 1] x [0, 1]
-    def __init__(self, fun, name, fun_type):
-    	# Initializing signals:
-    	# fun: a lambda 2D function
-    	# name: a name of target function
-    	# fun_type: a type of optimization problem (i.e. namy local optima, bowl shaped and so on)
+    # Main class to hold signal generating object
+    # It is supposed to hold objects from here: https://www.sfu.ca/~ssurjano/optimization.html
+    # It is assumed the function will hold its minimum of 0 and maximum of 1 
+    # The domain of the function would be [0, 1] x [0, 1]
+    def __init__(self, fun, x_opt, desc):
+        # Initializing signals:
+        # fun: a lambda 2D function
+        # x_opt: a location of minimum
+        # desc: a type of optimization problem (i.e. namy local optima, bowl shaped and so on)
         self.fun = fun
-        self.name = name
-        self.fun_type = fun_type
+        self.x_opt = x_opt
+        self.desc = desc
  
     def sample(self, x):
-    	return np.apply_along_axis(self.fun, 1, x)
+        return np.apply_along_axis(self.fun, 1, x)
 
 
 # A helper array to hold target signals
-TARGET_SIGNALS = [DataGenerator2D(lambda x: 0.05 * (-20 * np.exp(-0.2 * np.sqrt(0.5 * (64 * x[0] - 32) ** 2 + 0.5 * (64 * x[1] - 32) ** 2)) - np.exp(0.5 * np.cos(2 * np.pi * (64 * x[0] - 32)) + 0.5 * np.cos(2 * np.pi * (64 * x[1] - 32))) + 20 + np.exp(1)), "Ackley", "many_local_minima")
-				 ]
+# Be wary there is implicit links between normalization in functions are provided optimums
+TARGET_SIGNALS = [DataGenerator2D(ackley, np.array([0.5, 0.5]), "Many local minima: Ackley"),
+                  DataGenerator2D(bukin_n6, np.array([0.5, 0.66666]), "Many local minima: Bukin-N6"),
+                  # DataGenerator2D(cross_in_tray, np.array([[0.162725, 0.162725], [0.162725, 1 - 0.162725], [1 - 0.162725, 0.162725], [1 - 0.162725, 1 - 0.162725]]), "Many local minima: Cross-In-Tray"),
+                  DataGenerator2D(drop_wave, np.array([0.5, 0.5]), "Many local minima: Drop-Wave"),
+                  DataGenerator2D(eggholder, np.array([1.0, 0.89475]), "Many local minima: Eggholder"),
+                  DataGenerator2D(griewank, np.array([0.5, 0.5]), "Many local minima: Griewank"),
+                  # DataGenerator2D(holder, np.array([[0.0972, 0.0168], [0.0972, 1 - 0.0168], [1 - 0.0972, 0.0168], [1 - 0.0972, 1 - 0.0168]]), "Many local minima: Holder"),
+                  DataGenerator2D(levy, np.array([0.55, 0.55]), "Many local minima: Levy"),
+                  DataGenerator2D(levy_n13, np.array([0.55, 0.55]), "Many local minima: Levy-N13"),
+                  DataGenerator2D(rastrigin, np.array([0.5, 0.5]), "Many local minima: Rastrigin"),
+                  DataGenerator2D(schaffer_n2, np.array([0.5, 0.5]), "Many local minima: Schaffer-N2"),
+                  DataGenerator2D(schaffer_n4, np.array([0.5, 0.5]), "Many local minima: Schaffer-N4"),
+                  DataGenerator2D(schwefel, np.array([0.92096, 0.92096]), "Many local minima: Schwefel"),
+                  DataGenerator2D(bohachevsky, np.array([0.5, 0.5]), "Bowled shaped: Bohachevsky"),
+                  DataGenerator2D(perm_beta, np.array([0.75, 0.625]), "Bowled shaped: Perm-beta"),
+                  DataGenerator2D(sphere, np.array([0.5, 0.5]), "Bowled shaped: Sphere"),
+                  DataGenerator2D(power_sum, np.array([0.5, 0.5]), "Bowled shaped: Power-sum"),
+                  DataGenerator2D(square_sum, np.array([0.5, 0.5]), "Bowled shaped: Square-sum"),
+                  DataGenerator2D(trid, np.array([0.75, 0.75]), "Bowled shaped: Trid"),
+                  DataGenerator2D(matyas, np.array([0.5, 0.5]), "Plate shaped: Matyas"),
+                  DataGenerator2D(mccormick, np.array([0.173238, 0.207544]), "Plate shaped: McCormick"),
+                  DataGenerator2D(zakharov, np.array([0.5, 0.5]), "Plate shaped: Zakharov"),
+                  DataGenerator2D(three_hump_camel, np.array([0.5, 0.5]), "Valley shaped: Three-Hump-Camel"),
+                  DataGenerator2D(dixon_price, np.array([0.6, 0.57071]), "Valley shaped: Dixon-Price"),
+                  DataGenerator2D(rosenbrock, np.array([0.75, 0.75]), "Valley shaped: Rosenbrock"),
+                  DataGenerator2D(easom, np.array([0.5157, 0.5157]), "Drops: Easom"),
+                  DataGenerator2D(michalewicz, np.array([0.70028, 0.49974]), "Drops: Michalewicz"),
+                  DataGenerator2D(perm_d_beta, np.array([0.75, 1.0]), "Other: Perm-d-beta"),
+                  DataGenerator2D(styblinski_tang, np.array([0.2096, 0.2096]), "Other: Styblinski-Tang"),
+                  DataGenerator2D(beale, np.array([0.875, 0.5625]), "Other: Beale")
+                 ]
+
+
+
+
+
+
+
+
---
+++ sample_functions_2d.py
@@ -0,0 +1,321 @@
+
+import numpy as np
+
+# The file is supposed to hold the collection of 2D optimization problems
+# All of them are taken from here and tweaked a little bit: https://www.sfu.ca/~ssurjano/optimization.html
+# It is assumed all of them are defined over [0, 1] x [0, 1] range
+# All functions are normalized to have a minimum of 0
+# It is also assumed there is not much variance in function values (everything in ~[0, 2] range or so)
+
+
+def ackley(x):
+    x0 = 64 * x[0] - 32
+    x1 = 64 * x[1] - 32
+
+    a = 20
+    b = 0.2
+    c = 2 * np.pi
+
+    exp_1_term = np.exp(-b * np.sqrt(0.5 * x0 ** 2 + 0.5 * x1 ** 2))
+    exp_2_term = np.exp(0.5 * np.cos(c * x0) + 0.5 * np.cos(c * x1))
+
+    val = 0.05 * (-a * exp_1_term - exp_2_term + a * np.exp(1))
+
+    return val 
+
+
+def bukin_n6(x):
+    x0 = 10 * x[0] - 15
+    x1 = 6 * x[1] - 3
+
+    val = np.sqrt(np.abs(x1 - 0.01 * x0 ** 2)) + 0.0001 * np.abs(x0 + 10)
+
+    return val 
+
+
+def cross_in_tray(x):
+    x0 = 4 * x[0] - 2
+    x1 = 4 * x[1] - 2
+
+    val = 2.06261 - 0.0001 * np.power(np.abs(np.sin(x0) * np.sin(x1) * np.exp(np.abs(100 - np.sqrt(x0 ** 2 + x1 ** 2) / np.pi))) + 1, 0.1)
+
+    return val 
+
+
+def drop_wave(x):
+    x0 = 4 * x[0] - 2
+    x1 = 4 * x[1] - 2
+
+    norm = np.sqrt(x0 ** 2 + x1 ** 2)
+
+    val = 1 - (1 + np.cos(12 * norm)) / (0.5 * norm ** 2 + 2)
+
+    return val 
+
+
+def eggholder(x):
+    x0 = 1024 * x[0] - 512
+    x1 = 1024 * x[1] - 512
+
+    sin_1_term = np.sin(np.sqrt(np.abs(x1 + 0.5 * x0 + 47)))
+    sin_2_term = np.sin(np.sqrt(np.abs(x0 - x1 - 47)))
+
+    val = 0.001 * (959.6407 - (x1 + 47) * sin_1_term - x0 * sin_2_term)
+
+    return val 
+
+
+def griewank(x):
+    x0 = 1200 * x[0] - 600
+    x1 = 1200 * x[1] - 600
+
+    val = 0.01 * (x0 ** 2 / 4000.0 + x1 ** 2 / 4000.0 - np.cos(x0) - np.cos(x1 / np.sqrt(2)) + 1)
+
+    return val 
+
+
+def holder(x):
+    x0 = 20 * x[0] - 10
+    x1 = 20 * x[1] - 10
+
+    norm = np.sqrt(x0 ** 2 + x1 ** 2)
+    exp_term = np.exp(np.abs(1 - norm / np.pi))
+
+    val = 0.1 * (19.2085 - np.abs(np.sin(x0) * np.cos(x1) * exp_term))
+
+    return val 
+
+
+def levy(x):
+    x0 = 20 * x[0] - 10
+    x1 = 20 * x[1] - 10
+
+    w0 = 1 + 0.25 * (x0 - 1)
+    w1 = 1 + 0.25 * (x1 - 1)
+
+    sin_1_term = (w0 - 1) ** 2 * (1 + 10 * np.sin(np.pi * w0 + 1) ** 2)
+    sin_2_term = (w1 - 1) ** 2 * (1 + np.sin(2 * np.pi * w1) ** 2)
+
+    val = 0.02 * (np.sin(np.pi * w0) ** 2 + sin_1_term + sin_2_term)
+
+    return val 
+
+
+def levy_n13(x):
+    x0 = 20 * x[0] - 10
+    x1 = 20 * x[1] - 10
+
+    sin_1_term = (x0 - 1) ** 2 * (1 + np.sin(3 * np.pi * x1) ** 2)
+    sin_2_term = (x1 - 1) ** 2 * (1 + np.sin(2 * np.pi * x1) ** 2)
+
+    val = 0.005 * (np.sin(3 * np.pi * x0) ** 2 + sin_1_term + sin_2_term)
+
+    return val 
+
+
+def rastrigin(x):
+    x0 = 10 * x[0] - 5
+    x1 = 10 * x[1] - 5
+
+    sin_1_term = (x0 - 1) ** 2 * (1 + np.sin(3 * np.pi * x1) ** 2)
+    sin_2_term = (x1 - 1) ** 2 * (1 + np.sin(2 * np.pi * x1) ** 2)
+
+    val = 0.02 * (20 + x0 ** 2 + x1 ** 2 - 10 * np.cos(2 * np.pi * x0) - 10 * np.cos(2 * np.pi * x1))
+
+    return val 
+
+
+def schaffer_n2(x):
+    x0 = 200 * x[0] - 100
+    x1 = 200 * x[1] - 100
+
+    norm = np.sqrt(x0 ** 2 + x1 ** 2)
+    sin_term = np.sin(x0 ** 2 - x1 ** 2) ** 2
+
+    val = 0.5 + (sin_term - 0.5) / (np.abs(1 + 0.001 * norm ** 2) ** 2)
+
+    return val 
+
+
+def schaffer_n4(x):
+    x0 = 200 * x[0] - 100
+    x1 = 200 * x[1] - 100
+
+    norm = np.sqrt(x0 ** 2 + x1 ** 2)
+    cos_term = np.cos(np.sin(np.abs(x0 ** 2 - x1 ** 2)))
+
+    val = 0.5 + (cos_term - 0.5) / (np.abs(1 + 0.001 * norm ** 2) ** 2)
+
+    return val 
+
+
+def schwefel(x):
+    x0 = 1000 * x[0] - 500
+    x1 = 1000 * x[1] - 500
+
+    norm = np.sqrt(x0 ** 2 + x1 ** 2)
+    cos_term = np.cos(np.sin(np.abs(x0 ** 2 - x1 ** 2)))
+
+    val = 0.001 * (418.9829 * 2 - x0 * np.sin(np.sqrt(np.abs(x0))) - x1 * np.sin(np.sqrt(np.abs(x1)))) 
+
+    return val 
+
+
+def bohachevsky(x):
+    x0 = 200 * x[0] - 100
+    x1 = 200 * x[1] - 100
+
+    val = 0.0001 * (x0 ** 2 + 2 * x1 ** 2 - 0.3 * np.cos(3 * np.pi * x0) - 0.4 * np.cos(4 * np.pi * x1) + 0.7) 
+
+    return val 
+
+
+def perm_beta(x):
+    x0 = 4 * x[0] - 2
+    x1 = 4 * x[1] - 2
+
+    beta = 0
+
+    val = 0.1 * ((1 + beta) * (x0 - 1) ** 2 + (1 + beta) * (x0 ** 2 - 1) ** 2 + (2 + beta) * (x1 - 0.5) ** 2 + (2 + beta) * (x1 ** 2 - 0.25) ** 2)
+
+    return val 
+
+
+def sphere(x):
+    x0 = 10 * x[0] - 5
+    x1 = 10 * x[1] - 5
+
+    val = 0.1 * (x0 ** 2 + x1 ** 2)
+
+    return val 
+
+
+def power_sum(x):
+    x0 = 4 * x[0] - 2
+    x1 = 4 * x[1] - 2
+
+    val = 0.5 * (x0 ** 2 + x1 ** 3)
+
+    return val 
+
+
+def square_sum(x):
+    x0 = 10 * x[0] - 5
+    x1 = 10 * x[1] - 5
+
+    val = 0.1 * (x0 ** 2 + 2 * x1 ** 2)
+
+    return val 
+
+
+def trid(x):
+    x0 = 8 * x[0] - 4
+    x1 = 8 * x[1] - 4
+
+    val = 0.1 * (2 + (x0 - 1) ** 2 + (x1 - 1) ** 2 - x0 * x1)
+
+    return val 
+
+
+def matyas(x):
+    x0 = 10 * x[0] - 5
+    x1 = 10 * x[1] - 5
+
+    val = 0.05 * (0.26 * (x0 - 1) ** 2 + 0.26 * (x1 - 1) ** 2 - 0.48 * x0 * x1)
+
+    return val 
+
+
+def mccormick(x):
+    x0 = 5.5 * x[0] - 1.5
+    x1 = 7 * x[1] - 3
+
+    val = 0.1 * (1.9133 + np.sin(x0 + x1) + (x0 - x1) ** 2 - 1.5 * x0 + 2.5 * x1 + 1)
+
+    return val 
+
+
+def zakharov(x):
+    x0 = 10 * x[0] - 5
+    x1 = 10 * x[1] - 5
+
+    val = 0.01 * (x0 ** 2 + x1 ** 2 + (0.5 * x0 + x1) ** 2 + (0.5 * x0 + x1) ** 4)
+
+    return val 
+
+
+def three_hump_camel(x):
+    x0 = 10 * x[0] - 5
+    x1 = 10 * x[1] - 5
+
+    val = 0.001 * (2 * x0 ** 2 - 1.05 * x0 ** 4 + x0 ** 6 / 6.0 + x0 * x1 + x1 ** 2)
+
+    return val 
+
+
+def dixon_price(x):
+    x0 = 10 * x[0] - 5
+    x1 = 10 * x[1] - 5
+
+    val = 0.01 * ((x0 - 1) ** 2 + 2 * (2 * x1 ** 2 - x0) ** 2)
+
+    return val 
+
+
+def rosenbrock(x):
+    x0 = 4 * x[0] - 2
+    x1 = 4 * x[1] - 2
+
+    val = 0.001 * (100 * (x1 - x0 ** 2) ** 2 + (x0 - 1) ** 2)
+
+    return val 
+
+
+def easom(x):
+    x0 = 200 * x[0] - 100
+    x1 = 200 * x[1] - 100
+
+    val = 1 - np.cos(x0) * np.cos(x1) * np.exp(- (x0 - np.pi) ** 2 - (x1 - np.pi) ** 2)
+
+    return val 
+
+
+def michalewicz(x):
+    x0 = np.pi * x[0]
+    x1 = np.pi * x[1]
+
+    m = 10
+
+    val = 1.8013 - np.sin(x0) * np.sin(x0 ** 2 / np.pi) ** (2 * m) - np.sin(x1) * np.sin(2 * x1 ** 2 / np.pi) ** (2 * m)
+
+    return val 
+
+
+def perm_d_beta(x):
+    x0 = 4 * x[0] - 2
+    x1 = 4 * x[1] - 2
+
+    beta = 0
+
+    val = 0.02 * (((1 + beta) * (x0 - 1)  + (2 + beta) * (0.5 * x1 - 1)) ** 2 + ((1 + beta) * (x0 ** 2 - 1)  + (4 + beta) * (0.25 * x1 ** 2 - 1)) ** 2)
+
+    return val 
+
+
+def styblinski_tang(x):
+    x0 = 10 * x[0] - 5
+    x1 = 10 * x[1] - 5
+
+    val = 0.005 * (2 * 39.16599 + x0 ** 4 - 16 * x0 ** 2 + 5 * x0 + x1 ** 4 - 16 * x1 ** 2 + 5 * x1)
+
+    return val 
+
+
+def beale(x):
+    x0 = 8 * x[0] - 4
+    x1 = 8 * x[1] - 4
+
+    val = 0.001 * ((1.5 - x0 + x0 * x1) ** 2 + (2.25 - x0 + x0 * x1 ** 2) ** 2 + (2.625 - x0 + x0 * x1 ** 3) ** 2)
+
+    return val 
+
